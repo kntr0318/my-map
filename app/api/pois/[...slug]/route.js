31c352409b3f5fe3e68c4b671e4a174b7d476ae7
kntr0318 <kentarom.bz@gmail.com>
2025-09-09 14:34:17 +0900

    fic

--- app/api/pois/[...slug]/route.js
+++ app/api/pois/[...slug]/route.js
@@ -1,7 +1,5 @@
-// app/api/pois/[[...route]]/route.js
-import app from '../../../../api/pois/app.js';
-
 export const runtime = 'edge';
 
-export const GET  = (req) => app.fetch(req);
-export const POST = (req) => app.fetch(req);
+export async function GET(req, { params }) {
+  return Response.json({ ok: true, from: 'next-direct', params });
+}
