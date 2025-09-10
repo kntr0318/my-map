94cab853d43ef73a0a16848971a01bbaeafd8dd9
kntr0318 <kentarom.bz@gmail.com>
2025-09-10 18:34:16 +0900

    fix

--- app/api/[[...route]]/route.js
+++ app/api/[[...route]]/route.js
@@ -1,5 +1,5 @@
 // app/api/[[...route]]/route.js
-import app from '../../../api/pois/app.js'
+import app from '/api/pois/app.js'
 export const runtime = 'edge'
 export const GET = (req) => app.fetch(req)
 export const POST = GET
